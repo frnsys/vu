2827f3aa8981549b5dd42221cb1b174c0be6b399
Francis Tseng <f+accounts@frnsys.com>
2024-09-23 11:47:03 +0200

    feat: animated webp support

---
+++ src/img.rs
@@ -0,0 +1,109 @@
+use std::{fs::File, io::BufReader, path::Path};
+
+use image::{
+    codecs::{gif::GifDecoder, webp::WebPDecoder},
+    imageops::FilterType,
+    AnimationDecoder, GenericImageView, ImageDecoder,
+};
+
+/// We can have either a single image
+/// or a sequence of images (i.e. an animated gif).
+pub enum Image {
+    Single {
+        data: Vec<u8>,
+        size: (u32, u32),
+    },
+    Sequence {
+        frames: Vec<Vec<u8>>,
+        delays: Vec<f64>,
+        index: usize,
+        size: (u32, u32),
+    },
+}
+impl Image {
+    pub fn size(&self) -> (u32, u32) {
+        match self {
+            Self::Single { size, .. } => *size,
+            Self::Sequence { size, .. } => *size,
+        }
+    }
+
+    pub fn delays(&self) -> Option<&[f64]> {
+        match self {
+            Image::Sequence { delays, .. } => Some(delays),
+            _ => None,
+        }
+    }
+
+    /// Get the next frame.
+    pub fn next_frame(&mut self) -> &[u8] {
+        match self {
+            Self::Single { data, .. } => data,
+            Self::Sequence { index, frames, .. } => {
+                let data = &frames[*index % frames.len()];
+                *index += 1;
+                data
+            }
+        }
+    }
+}
+
+/// Read frames from an animated format.
+fn read_frames<'a, D: AnimationDecoder<'a> + ImageDecoder>(decoder: D) -> Image {
+    let size = decoder.dimensions();
+    let decoded = decoder
+        .into_frames()
+        .collect_frames()
+        .expect("Failed to decode frames");
+    let (frames, delays): (Vec<_>, Vec<_>) = decoded
+        .into_iter()
+        .map(|f| {
+            let (num, den) = f.delay().numer_denom_ms();
+            let delay = (num as f64 / den as f64) / 1000.;
+            (f.into_buffer().into_raw(), delay)
+        })
+        .unzip();
+
+    Image::Sequence {
+        frames,
+        delays,
+        size,
+        index: 0,
+    }
+}
+
+pub fn read_image(path: &Path, (max_width, max_height): (u32, u32)) -> Option<Image> {
+    let ext = path.extension().and_then(|ext| ext.to_str());
+    match ext {
+        Some("gif") => {
+            let file = File::open(path).expect("Failed to read gif");
+            let reader = BufReader::new(file);
+            let decoder = GifDecoder::new(reader).expect("Failed to decode gif");
+            Some(read_frames(decoder))
+        }
+        Some("webp") => {
+            let file = File::open(path).expect("Failed to read webp");
+            let reader = BufReader::new(file);
+            let decoder = WebPDecoder::new(reader).expect("Failed to decode webp");
+            Some(read_frames(decoder))
+        }
+        _ => {
+            match image::open(path) {
+                Ok(mut img) => {
+                    // Resize to fit if needed.
+                    let size = img.dimensions();
+                    if size.0 > max_width || size.1 > max_height {
+                        img = img.resize(max_width, max_height, FilterType::Triangle);
+                    }
+                    let rgba = img.to_rgba8();
+                    let pixels: Vec<u8> = rgba.into_raw();
+                    Some(Image::Single { data: pixels, size })
+                }
+                Err(e) => {
+                    eprintln!("Failed to load image: {}", e);
+                    None
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
+mod img;
+
 use std::{
-    fs::File,
-    io::BufReader,
     path::PathBuf,
     process::exit,
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
     },
-    thread,
+    thread::{self, JoinHandle},
     time::Duration,
 };
 
-use image::{
-    codecs::gif::GifDecoder, imageops::FilterType, AnimationDecoder, GenericImageView, ImageDecoder,
-};
 use pixels::{wgpu::Color, Error, PixelsBuilder, SurfaceTexture};
 use winit::{
     dpi::{LogicalSize, PhysicalSize},
@@ -28,23 +25,6 @@ const MARGIN: u32 = 50;
 
 /// Transparent/background color.
 const CLEAR_COLOR: Color = Color::BLUE;
-
-/// We can have either a single image
-/// or a sequence of images (i.e. an animated gif).
-enum Image<'a> {
-    Single(&'a [u8]),
-    Sequence(Vec<&'a [u8]>),
-}
-impl Image<'_> {
-    /// Get a frame by index.
-    /// This automatically wraps the index around.
-    fn get_frame(&self, idx: usize) -> &[u8] {
-        match self {
-            Self::Single(data) => data,
-            Self::Sequence(frames) => frames[idx % frames.len()],
-        }
-    }
-}
 
 /// Event emitted when the next frame in
 /// an image sequence should be shown.
@@ -81,10 +61,6 @@ fn main() -> Result<(), Error> {
         }
     };
 
-    // Use this as a flag to indicate when the frame delay thread
-    // should terminate.
-    let is_running = Arc::new(AtomicBool::new(true));
-
     let event_loop = EventLoopBuilder::<RequestNextFrame>::with_user_event()
         .build()
         .expect("Failed to create event loop");
@@ -96,32 +72,40 @@ fn main() -> Result<(), Error> {
         .expect("Couldn't find the primary monitor");
     let mon_size = mon.size();
     let scale_factor = mon.scale_factor();
-    let (mon_width, mon_height) = (mon_size.width - MARGIN * 2, mon_size.height - MARGIN * 2);
+    let (max_width, max_height) = (mon_size.width - MARGIN * 2, mon_size.height - MARGIN * 2);
 
-    // Handle gifs.
-    if args.path.extension().is_some_and(|ext| ext == "gif") {
-        // Load and extract all the frames.
-        let file = File::open(args.path).expect("Failed to read gif");
-        let reader = BufReader::new(file);
-        let decoder = GifDecoder::new(reader).expect("Failed to decode gif");
-        let size = decoder.dimensions();
-        let decoded = decoder
-            .into_frames()
-            .collect_frames()
-            .expect("Failed to decode frames");
-        let (frames, delays): (Vec<_>, Vec<_>) = decoded
-            .iter()
-            .map(|f| {
-                let (num, den) = f.delay().numer_denom_ms();
-                let delay = (num as f64 / den as f64) / 1000.;
-                (f.buffer().as_raw().as_slice(), delay)
-            })
-            .unzip();
+    let window = Window {
+        title: &args.title,
+        scale_factor,
+        should_focus: !args.no_focus,
+    };
+
+    let image = img::read_image(&args.path, (max_width, max_height));
+    match image {
+        Some(image) => {
+            window.display(event_loop, image)?;
+        }
+        None => exit(1),
+    }
+    Ok(())
+}
+
+struct Animator {
+    handle: Option<JoinHandle<()>>,
+
+    /// A flag indicating when the frame delay thread
+    /// should terminate.
+    is_running: Arc<AtomicBool>,
+}
+impl Animator {
+    fn new(event_loop: &EventLoop<RequestNextFrame>, delays: &[f64]) -> Self {
+        let is_running = Arc::new(AtomicBool::new(true));
 
         // Setup a separate thread to handle frame
         // delays/advancement.
         let should_run = Arc::clone(&is_running);
         let proxy = event_loop.create_proxy();
+        let delays = delays.to_vec();
         let handle = thread::spawn(move || {
             'outer: while should_run.load(Ordering::SeqCst) {
                 for delay in &delays {
@@ -133,140 +117,116 @@ fn main() -> Result<(), Error> {
             }
         });
 
-        run(
-            event_loop,
-            &args.title,
-            Image::Sequence(frames),
-            !args.no_focus,
-            size,
-            scale_factor,
-        )?;
-
-        // We're quitting, let the thread know
-        // and wait for it to finish.
-        is_running.store(false, Ordering::SeqCst);
-        handle.join().unwrap();
-
-    // Display a single static image.
-    } else {
-        match image::open(args.path) {
-            Ok(mut img) => {
-                // Resize to fit if needed.
-                let size = img.dimensions();
-                if size.0 > mon_width || size.1 > mon_height {
-                    img = img.resize(mon_width, mon_height, FilterType::Triangle);
-                }
-                let rgba = img.to_rgba8();
-                let pixels: &[u8] = rgba.as_raw();
-                run(
-                    event_loop,
-                    &args.title,
-                    Image::Single(pixels),
-                    !args.no_focus,
-                    img.dimensions(),
-                    scale_factor,
-                )?;
-            }
-            Err(e) => {
-                eprintln!("Failed to load image: {}", e);
-                exit(1);
-            }
+        Self {
+            is_running,
+            handle: Some(handle),
         }
     }
-
-    Ok(())
+}
+impl Drop for Animator {
+    fn drop(&mut self) {
+        self.is_running.store(false, Ordering::SeqCst);
+        self.handle.take().unwrap().join().unwrap();
+    }
 }
 
-fn run(
-    event_loop: EventLoop<RequestNextFrame>,
-    title: &str,
-    img: Image,
-    focus: bool,
-    (width, height): (u32, u32),
+struct Window<'a> {
+    title: &'a str,
+    should_focus: bool,
     scale_factor: f64,
-) -> Result<(), Error> {
-    let size = PhysicalSize::new(width as f64, height as f64);
+}
+impl Window<'_> {
+    fn display(
+        &self,
+        event_loop: EventLoop<RequestNextFrame>,
+        mut img: img::Image,
+    ) -> Result<(), Error> {
+        let (width, height) = img.size();
+        let size = PhysicalSize::new(width as f64, height as f64);
 
-    // Note: For Wayland positioning has to happen via the window manager.
-    let window = WindowBuilder::new()
-        .with_title(title)
-        .with_resizable(false)
-        .with_decorations(false)
-        .with_window_level(WindowLevel::AlwaysOnTop)
-        .with_inner_size(LogicalSize::<f64>::from_physical(size, scale_factor))
-        .build(&event_loop)
-        .unwrap();
+        // Note: For Wayland positioning has to happen via the window manager.
+        let window = WindowBuilder::new()
+            .with_title(self.title)
+            .with_resizable(false)
+            .with_decorations(false)
+            .with_window_level(WindowLevel::AlwaysOnTop)
+            .with_inner_size(LogicalSize::<f64>::from_physical(size, self.scale_factor))
+            .build(&event_loop)
+            .unwrap();
 
-    let surface_texture = SurfaceTexture::new(width, height, &window);
-    let mut pixels = PixelsBuilder::new(width, height, surface_texture)
-        .clear_color(CLEAR_COLOR)
-        .build()?;
-    pixels.frame_mut().copy_from_slice(img.get_frame(0));
+        let surface_texture = SurfaceTexture::new(width, height, &window);
+        let mut pixels = PixelsBuilder::new(width, height, surface_texture)
+            .clear_color(CLEAR_COLOR)
+            .build()?;
+        pixels.frame_mut().copy_from_slice(img.next_frame());
 
-    let mut frame_idx = 0;
-    event_loop
-        .run(move |event, target| match event {
-            // Go to the next frame in a sequence.
-            Event::UserEvent(RequestNextFrame) => {
-                frame_idx += 1;
-                let data = img.get_frame(frame_idx);
-                pixels.frame_mut().copy_from_slice(data);
-                if pixels.render().is_err() {
-                    target.exit();
-                }
-            }
-            Event::WindowEvent {
-                event: WindowEvent::RedrawRequested,
-                ..
-            } => {
-                if pixels.render().is_err() {
-                    target.exit();
-                }
-            }
-            Event::WindowEvent {
-                event: WindowEvent::Focused(true),
-                ..
-            } => {
-                // This is a super-hacky, river-specific way
-                // of making the window unfocusable. Basically
-                // it punts focus back to whatever previously had focus.
-                // Ideally `winit` gets some better support for Wayland features;
-                // for example, in `sema` I use the `gtk` crate which lets you
-                // make a window unfocusable; I just don't want to port everything
-                // to that and this works ok for now.
-                if !focus {
-                    std::process::Command::new("riverctl")
-                        .args(["focus-view", "previous"])
-                        .output()
-                        .unwrap();
-                }
-            }
+        let _animator = img
+            .delays()
+            .map(|delays| Animator::new(&event_loop, delays));
 
-            // Exit on Esc or Q
-            Event::WindowEvent {
-                event:
-                    WindowEvent::KeyboardInput {
-                        event:
-                            KeyEvent {
-                                physical_key: PhysicalKey::Code(key),
-                                ..
-                            },
-                        ..
-                    },
-                ..
-            } => match key {
-                KeyCode::Escape | KeyCode::KeyQ => {
-                    target.exit();
+        event_loop
+            .run(move |event, target| match event {
+                // Go to the next frame in a sequence.
+                Event::UserEvent(RequestNextFrame) => {
+                    let data = img.next_frame();
+                    pixels.frame_mut().copy_from_slice(data);
+                    if pixels.render().is_err() {
+                        target.exit();
+                    }
                 }
+                Event::WindowEvent {
+                    event: WindowEvent::RedrawRequested,
+                    ..
+                } => {
+                    if pixels.render().is_err() {
+                        target.exit();
+                    }
+                }
+                Event::WindowEvent {
+                    event: WindowEvent::Focused(true),
+                    ..
+                } => {
+                    // This is a super-hacky, river-specific way
+                    // of making the window unfocusable. Basically
+                    // it punts focus back to whatever previously had focus.
+                    // Ideally `winit` gets some better support for Wayland features;
+                    // for example, in `sema` I use the `gtk` crate which lets you
+                    // make a window unfocusable; I just don't want to port everything
+                    // to that and this works ok for now.
+                    if !self.should_focus {
+                        std::process::Command::new("riverctl")
+                            .args(["focus-view", "previous"])
+                            .output()
+                            .unwrap();
+                    }
+                }
+
+                // Exit on Esc or Q
+                Event::WindowEvent {
+                    event:
+                        WindowEvent::KeyboardInput {
+                            event:
+                                KeyEvent {
+                                    physical_key: PhysicalKey::Code(key),
+                                    ..
+                                },
+                            ..
+                        },
+                    ..
+                } => match key {
+                    KeyCode::Escape | KeyCode::KeyQ => {
+                        target.exit();
+                    }
+                    _ => (),
+                },
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => target.exit(),
                 _ => (),
-            },
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => target.exit(),
-            _ => (),
-        })
-        .expect("Event loop failed");
+            })
+            .expect("Event loop failed");
 
-    Ok(())
+        Ok(())
+    }
 }
